86ce51549bd4db80611096583f1a1a9dc0583c49
Géza Tóth <geza.toth@fun-save.com>
2022-01-03 13:32:55 +0100

    add files

---
+++ datepicker.js
@@ -0,0 +1,58 @@
+$(function () {
+    $.fn.datepicker.dates["hu"] = {
+        days: [
+            "vasárnap",
+            "hétfő",
+            "kedd",
+            "szerda",
+            "csütörtök",
+            "péntek",
+            "szombat",
+        ],
+        daysShort: ["vas", "hét", "ked", "sze", "csü", "pén", "szo"],
+        daysMin: ["V", "H", "K", "Sze", "Cs", "P", "Szo"],
+        months: [
+            "január",
+            "február",
+            "március",
+            "április",
+            "május",
+            "június",
+            "július",
+            "augusztus",
+            "szeptember",
+            "október",
+            "november",
+            "december",
+        ],
+        monthsShort: [
+            "jan",
+            "feb",
+            "már",
+            "ápr",
+            "máj",
+            "jún",
+            "júl",
+            "aug",
+            "sze",
+            "okt",
+            "nov",
+            "dec",
+        ],
+        today: "ma",
+        weekStart: 1,
+        clear: "Töröl",
+        titleFormat: "yyyy. MM",
+        format: "yyyy.mm.dd",
+    };
+    $("#datepicker").datepicker({
+        // format: "yyyy-mm-dd",
+        startView: 2,
+        clearBtn: true,
+        language: "hu",
+        autoclose: true,
+        toggleActive: true,
+        endDate: '-18y'
+        // defaultViewDate: { year: 2003, month: 04, day: 25 },
+    });
+});
---
+++ main.js
@@ -0,0 +1,255 @@
+// import { datepicker } from "datepicker.js"
+
+$(function () {
+    console.log(navigator.userAgent)
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        // true for mobile device
+        console.log("mobile device");
+    } else {
+        // false for not mobile device
+        console.log("not mobile device");
+    }
+    $("#headingTwoButton").on('click', function () {
+        // console.log(dateValidation('#dateInput'));
+        // console.log("telszám: " + $('#phoneInput').val().replace(/[-+()\s]/g, ''))
+        setNameValidationMessage('#lastNameInput');
+        setNameValidationMessage('#firstNameInput');
+        setDateValidationMessage();
+        if ($('#phoneInput').val() != '') {
+            setPhoneValidationMessage()
+        } else {
+            $('#phoneInput').removeClass('is-invalid').removeClass('is-valid');
+        }
+        if (nameValidation('#firstNameInput') && nameValidation('#lastNameInput') && dateValidation('#dateInput') && (phoneValidation() || $('#phoneInput').val() === '')) {
+            $('#collapseOne').collapse();
+            $('#collapseTwo').collapse();
+            $('#headingTwo').remove();
+        }
+
+    })
+    var phoneMask = IMask(
+        document.getElementById('phoneInput'), {
+        mask: '(00) 000-0000',
+    });
+
+    $("#datepicker").datepicker().on('changeDate', function (ev) {
+        $('#dateInput').change();
+    });
+
+    $('#dateInput').change(function () {
+        setDateValidationMessage();
+    });
+
+    $('#firstNameInput').on('blur input', function () {
+        setNameValidationMessage('#firstNameInput');
+    });
+
+    $('#lastNameInput').on('blur input', function () {
+        setNameValidationMessage('#lastNameInput');
+    });
+
+    $('#phoneInput').on('blur input', function () {
+        if ($('#phoneInput').val() != '') {
+            setPhoneValidationMessage()
+        } else {
+            $('#phoneInput').removeClass('is-invalid').removeClass('is-valid');
+        }
+    });
+
+    $('#passwordInput').on('blur input', function () {
+        setPasswordValidationMessage();
+        if ($('#confirmPasswordInput').val() !== ''){
+            setConfirmPasswordValidationMessage()
+        }
+    });
+
+    $('#confirmPasswordInput').on('blur input', function () {
+        setConfirmPasswordValidationMessage()
+    })
+
+
+    function setNameValidationMessage(nameId) {
+        if (nameValidation(nameId)) {
+            $(nameId).removeClass('is-invalid').removeClass('remove-ok').addClass('is-valid').addClass('glyphicon-ok');
+        } else {
+            $(nameId).removeClass('is-valid').removeClass('glyphicon-ok').addClass('is-invalid').addClass('glyphicon-remove');
+        }
+    }
+
+    function setDateValidationMessage() {
+        if (dateValidation('#dateInput')) {
+            $('#dateInput').removeClass('is-invalid').addClass('is-valid');
+        } else {
+            $('#dateInput').removeClass('is-valid').addClass('is-invalid');
+        }
+    }
+
+    function setPhoneValidationMessage() {
+        if (phoneValidation()) {
+            $('#phoneInput').removeClass('is-invalid').addClass('is-valid');
+        } else {
+            $('#phoneInput').removeClass('is-valid').addClass('is-invalid');
+        }
+    }
+    function setPasswordValidationMessage() {
+        if (passwordValidation()) {
+            $('#passwordInput').removeClass('is-invalid').addClass('is-valid');
+        } else {
+            // console.log(/^\S*$ /.test($('#passwordInput').val()));
+            if (/^\S*$/.test($('#passwordInput').val())) {
+                $("#pass-required").html("Kérjük, helyesen töltsd ki ezt a mezőt!")
+            } else {
+                $("#pass-required").html("A jelszó nem tartalmazhat szóközt!")
+            }
+            $('#passwordInput').removeClass('is-valid').addClass('is-invalid');
+        }
+
+    }
+
+    function setConfirmPasswordValidationMessage() {
+        if (confirmPasswordValidation()) {
+            $('#confirmPasswordInput').removeClass('is-invalid').addClass('is-valid');
+        } else {
+            $('#confirmPasswordInput').removeClass('is-valid').addClass('is-invalid');
+        }
+    }
+
+    function nameValidation(id) {
+        return $(id).val().trim() !== ''
+    }
+
+    function dateValidation(dateId) {
+        var birthString = $(dateId).val().split(".");
+        var birthDate = new Date((parseInt(birthString[0]) + 18), parseInt(birthString[1]) - 1, birthString[2])
+        var today = new Date();
+        return today >= birthDate;
+    }
+
+    function phoneValidation() {
+        return $('#phoneInput').val().replace(/-|\(|\)|\ /g, '').length == 9;
+    }
+
+    function confirmPasswordValidation() {
+        return $('#passwordInput').val() === $('#confirmPasswordInput').val();
+    }
+
+    // Password
+    let passwordInput = $('#passwordInput');
+    let letter = $('#letter');
+    let capital = $('#capital');
+    let number = $('#number');
+    let length = $('#length');
+    let message = $('#message');
+    let names = $('#names');
+
+    // When the user clicks on the password field, show the message box
+    passwordInput.focus(function () {
+        message.collapse('show');
+        // $('#message').show();
+    })
+
+    // When the user clicks outside of the password field, hide the message box
+    passwordInput.blur(function () {
+        message.collapse('hide');
+        // $('#message').hide();
+
+    })
+    // When the user starts to type something inside the password field
+    passwordInput.keyup(function () {
+
+        // Validate lowercase letters
+        var lowerCaseLetters = /[a-z]/g;
+        if (passwordInput.val().match(lowerCaseLetters)) {
+            letter.removeClass("invalid");
+            letter.addClass("valid");
+        } else {
+            letter.removeClass("valid");
+            letter.addClass("invalid");
+        }
+        // Validate capital letters
+        var upperCaseLetters = /[A-Z]/g;
+        if (passwordInput.val().match(upperCaseLetters)) {
+            capital.removeClass("invalid");
+            capital.addClass("valid");
+        } else {
+            capital.removeClass("valid");
+            capital.addClass("invalid");
+        }
+
+        // Validate numbers
+        var numbers = /[0-9]/g;
+        if (passwordInput.val().match(numbers)) {
+            number.removeClass("invalid");
+            number.addClass("valid");
+        } else {
+            number.removeClass("valid");
+            number.addClass("invalid");
+        }
+
+        // Validate length
+        if (passwordInput.val().length >= 8 && passwordInput.val().length <= 15) {
+            length.removeClass("invalid").addClass("valid");
+        } else {
+            length.removeClass("valid").addClass("invalid");
+        }
+
+        // Validate name similarity
+        if (passwordNotMatchName()) {
+            names.removeClass("invalid").addClass("valid");
+        } else {
+            names.removeClass("valid").addClass("invalid");
+        }
+    })
+    
+    var makeSortString = (function () {
+        var translate_re = /[éáűőúöüóíÉÁŰPŐÚÖÜÓÍ]/g;
+        var translate = {
+            "é": "e", "á": "a", "ű": "u", "ő": "o", "ú": "u",
+            "ö": "o", "ü": "u", "ó": "o", "í": "i", "É": "E",
+            "Á": "A", "Ű": "U", "Ő": "O", "Ú": "U", "Ö": "O",
+            "Ü": "U", "Ó": "O", "Í": "I"
+        };
+        return function (s) {
+            return (s.replace(translate_re, function (match) {
+                return translate[match];
+            }));
+        }
+    })();
+
+    function passwordValidation() {
+        let regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[^-\s]{8,15}$/;
+        let password = document.getElementById('passwordInput').value;
+        return passwordNotMatchName() && password.match(regex);
+    }
+
+    function passwordNotMatchName() {
+        let matchArray = [];
+        // console.log("password: " + $('#passwordInput').val());
+        let email = $("#emailInput").val();
+        let firstName = $('#firstNameInput').val().trim();
+        let lastName = $('#lastNameInput').val().trim();
+        let password = document.getElementById('passwordInput').value;
+
+        let nameOfEmail = email.substring(0, email.indexOf('@'));
+        // if email contains more name (with dot)
+        if (nameOfEmail.includes(".")) {
+            matchArray = matchArray.concat(nameOfEmail.split('.').map(names => makeSortString(names)));
+        } else {
+            matchArray.push(makeSortString(nameOfEmail))
+        }
+        // if lastname contains more name
+        if (lastName.includes(" ")) {
+            matchArray = matchArray.concat(lastName.split(' ').map(names => makeSortString(names)));
+        } else {
+            matchArray.push(makeSortString(lastName))
+        }
+        // if first name contains more name
+        if (firstName.includes(" ")) {
+            matchArray = matchArray.concat(firstName.split(' ').map(names => makeSortString(names)));
+        } else {
+            matchArray.push(makeSortString(firstName))
+        }
+        // console.log(matchArray);
+        return !matchArray.some(name => makeSortString(password.toLowerCase()).includes(name.toLowerCase()));
+    }
+})
